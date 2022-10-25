d88aa251b4466429eca91ed6a89855616a06d0a2
Anurag Suraj Bhardwaj <105678749+BhardwajAnurag@users.noreply.github.com>
2022-10-25 19:56:12 +0000

    day3 - created a recipe generator

---
+++ day3_100days.py
@@ -0,0 +1,8 @@
+food = input("Name a food ")
+plant = input("Name a type of plant ")
+method = input("Name a method of cooking ")
+describe = input("What word describes burned food? ")
+diyItem = input("Name a DIY item ")
+print()
+print("MENU")
+print(method, food, "with", describe, plant, "on a bed of", diyItem)
