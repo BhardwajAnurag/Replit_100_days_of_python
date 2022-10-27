85388213f9298a39479d1e86497c952de350f463
Anurag Suraj Bhardwaj <105678749+BhardwajAnurag@users.noreply.github.com>
2022-10-27 16:04:58 +0000

    day6_100days created My Login System

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Replit_100_days_of_python
 Day1: Understood about strings and quotes <br />
-Day2: Understood about input, assigning it to variable and printing variables
-Day3: Created Project#1 a recipe generator using concatenation
-Day4: Created Project#2 custom story creator with colorful text output
-Day5: Learned about if else statements and created Project#3 Marvel Movie Character Creator
+Day2: Understood about input, assigning it to variable and printing variables <br />
+Day3: Created Project#1 a recipe generator using concatenation <br />
+Day4: Created Project#2 custom story creator with colorful text output <br />
+Day5: Learned about if else statements and created Project#3 Marvel Movie Character Creator <br />
+Day6: Learned about "elif" and "and" and created Project#4 My Login System <br />
---
+++ day6_100days.py
@@ -0,0 +1,44 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+username = input("Username > ")
+password = input("Password > ")
+
+if username == "david" and password == "davidp":
+    print(
+        "Why hello there David, what a lovely accent you have, you coul have charmed your way in here even without a password."
+    )
+    print()
+    print("Have a great day")
+    print()
+    print("Don't forget to wear a hat in the sun")
+elif username == "mark" and password == "markp":
+    print(
+        "Welcome to the other side of login. your outfit needs to be changed")
+elif username == "bob" and password == "bobp":
+    print("Bob we missed you, welcome back!")
+else:
+    print("Unauthorized access! Get out before we call the cops!!")
+
+# season = input("what is your favorite season? ")
+# if season == "spring":
+#   print("Ah! The birds are chirping and flowers blooming.")
+# elif season == "summer":
+#   print("Catch some sun and cool off with a lemonade.")
+# elif season == "autumn":
+#   print("The leaves are changing and the air is crisp. Enjoy!")
+# elif season == "winter":
+#   print("Stay warm by the fire and watch the snow fall.")
+# else:
+#   print("I don't know that season. Please try again.")
+# --------------------------------------------------------
+
+# print("SECURE LOGIN")
+# username = input("Username > ")
+# password = input("Password> ")
+
+# if username == "mark" and password == "password":
+#  print("Welcome Mark!")
+# elif username == "suzanne" and password == "pass":
+#  print("Hey there Suzanne!")
+# else:
+#  print("Go away!")
