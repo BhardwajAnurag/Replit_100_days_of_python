4db0d3bda1772816cbc808d4233feaeab76f6219
Anurag Suraj Bhardwaj <105678749+BhardwajAnurag@users.noreply.github.com>
2022-10-29 19:36:34 +0000

    day7_100days_week1_complete_tvShowFanFinder

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Replit_100_days_of_python
+This repo has Week 1 (Day 1 to Day 7) of follow along and challenges of the 100 days of code replit challenge in python <br/>
 Day1: Understood about strings and quotes <br />
 Day2: Understood about input, assigning it to variable and printing variables <br />
 Day3: Created Project#1 a recipe generator using concatenation <br />
 Day4: Created Project#2 custom story creator with colorful text output <br />
 Day5: Learned about if else statements and created Project#3 Marvel Movie Character Creator <br />
 Day6: Learned about "elif" and "and" and created Project#4 My Login System <br />
-Day7: End of week
+Day7: Learned about nesting and created a TV Show Fan Finder!
--- day7_100days.py
+++ day7_100days.py
@@ -1,14 +1,51 @@
-print("Are you a superfan of 'The Big Bang Theory' or a fake fan?")
-print()
-print("Answer these questions to find out.")
-
-Glasses = input("Does someone wear glasses?")
-if Glasses == "yes":
-    print("Correct!")
-else:
-    print("Wrong!")
-    WhoGlasses = input("And who wears glasses?")
-    if WhoGlasses == "Leonard":
-        print("You got it")
+print("Fake Fan Finder")
+print("----------------------------")
+anime = input("""What's your Favourite Anime? 
+""")
+if anime == "One piece":
+  character = input("Oh really? Tell me a few of the characters \n")
+  if character == "Nami":
+    job = input("You got this by pure chance. Tell me what does she do on the ship?")
+    if job == "Navigator":
+        bounty = input("Hmm! What was her first bounty then?")
+        if bounty == "money":
+            print("You are true fan")
+        else:
+            print("Gotcha! A FAKE one piece fan!")
     else:
-        print("Try again!")
+        print("Gotcha! A FAKE one piece fan!")
+  else:
+    print("Gotcha! A FAKE one piece fan!")
+else:
+  print("Gotcha! A FAKE one piece fan!")
+          
+# order = input("What would you like to order: pizza or hamburger? ")
+# if order == "hamburger":
+#   print("Thank you.")
+#   cheese = input("Do you want cheese? ")
+#   if cheese == "yes":
+#     print("You got it.")
+#   else: 
+#     print("No cheese it is.")
+# elif order == "pizza":
+#   print("Pizza coming up.")
+#   toppings = input("Do you want pepperoni on that? ")
+#   if toppings == "yes":
+#     print("We will add pepperoni.")
+#   else:
+#     print("Your pizza will not have pepperoni.")
+
+# tvShow = input("What is your favourite tv show? ")
+# if tvShow == "the office":
+#   print("Ugh, why?")
+#   faveCharacter = input("Who is your favourite character? ")
+#   if faveCharacter == "jim":
+#     print("he is tall")
+#   elif faveCharacter == "dwight":
+#     print("he has short nose")
+#   else:
+#     print("bad choice")
+# elif tvShow == "friends":
+#   print("Aww, it's very old")
+# else:
+#   print("Yeah, that's cool and all…")
